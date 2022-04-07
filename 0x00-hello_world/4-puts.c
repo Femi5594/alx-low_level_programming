1b9fd19427b5b9a37a894ee2424fd7edfd637531
femi5594 <radewumi5594@gmail.com>
2022-04-07 14:40:29 -0700

    Task 3-6

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - Entry point for all C programs
+ * Return: Returns 0 when the command is succesful
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C programming entry point
+ * Return: returns 0 if succesful
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

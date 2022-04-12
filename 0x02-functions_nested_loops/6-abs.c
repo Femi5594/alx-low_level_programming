d1087a54d1fc0ecfb099b3f3385534bc5ccbd767
femi5594 <radewumi5594@gmail.com>
2022-04-12 13:49:53 -0700

    Task 3-6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _abs - convert - to +
+ * @n: parameter
+ * Return: a value
+ */
+
+int _abs(int n)
+
+{
+
+	if (n < 0)
+
+	{
+
+		n *= -1;
+
+		return (n);
+
+	}
+
+	else
+
+	{
+
+		return (n);
+
+	}
+
+}

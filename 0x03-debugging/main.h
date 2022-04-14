f467a83c0063510f822ff9fcfd55da280c329e4f
femi5594 <radewumi5594@gmail.com>
2022-04-14 16:39:28 -0700

    Fixed task 0 & 1

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,13 +1,3 @@
-#ifndef MAIN_H
-
-#define MAIN_H
-
-
-
-#include <stdio.h>
-
-
-
 void positive_or_negative(int i);
 
 int largest_number(int a, int b, int c);
@@ -15,7 +5,3 @@ int largest_number(int a, int b, int c);
 void print_remaining_days(int month, int day, int year);
 
 int convert_day(int month, int day);
-
-
-
-#endif /* MAIN_H */

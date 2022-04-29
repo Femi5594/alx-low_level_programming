11a86f79b79784131390564e6b2b44002f2f7fde
femi5594 <radewumi5594@gmail.com>
2022-04-28 17:37:29 -0700

    Task 1-4

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * factorial - factorial
+ * @n: param
+ * Return: int
+ */
+
+int factorial(int n)
+
+{
+
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 1)
+	{
+		return (n);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}

dab006c1aa8a9a326c1f72d993692662170e96b1
femi5594 <radewumi5594@gmail.com>
2022-05-09 14:59:27 -0700

    Advanced task-101

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,7 +20,6 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
@@ -31,7 +30,6 @@ int main(int argc, char *argv[])
 				exit(98);
 			}
 		}
-
 	}
 	mul = atol(argv[1]) *  atol(argv[2]);
 	printf("%lu\n", mul);
